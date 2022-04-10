a557bb224a4e2c8f044a11a0e6abe66c5de41d29
Dick Dijk <dick@dijkrosoft.nl>
2022-04-10 12:11:21 +0200

    fix format

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
-{
-  "jest": {
-    "coverageThreshold": {
-      "global": {
-        "branches": 80,
-        "functions": 80,
-        "lines": 80,
-        "statements": -10
-      }
+const config = {
+  verbose: true,
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": -10
     }
   }
-}
+};
+
+module.exports = config;
