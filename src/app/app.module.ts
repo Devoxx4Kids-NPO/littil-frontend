ee906e33bf1fe1066fc808b6fb2182f211140ad0
Wilko Zonnenberg <wilko.zonnenberg@crv4all.com>
2022-06-07 23:14:37 +0200

    add "testcall" to make sure token is sent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalControllerModule } from './components/modal/modal.controller.module';
-import { AuthModule } from '@auth0/auth0-angular';
+import { AuthModule, AuthHttpInterceptor } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,10 +18,30 @@ import { AuthModule } from '@auth0/auth0-angular';
     AuthModule.forRoot({
       // TODO make configurable to allow more environments
       domain: 'dev-g60bne29.eu.auth0.com',
-      clientId: '1MWGJlOHqjqNHiPZKEY5R4C7fsQySr9k'
+      clientId: '1MWGJlOHqjqNHiPZKEY5R4C7fsQySr9k',
+      // todo set proper audience uri
+      audience: 'https://dev-g60bne29.eu.auth0.com/api/v2/',
+
+      // Specify configuration for the interceptor
+      httpInterceptor: {
+        allowedList: [
+          {
+            // Match any request that starts 'https://dev-g60bne29.eu.auth0.com/api/v2/' (note the asterisk)
+            // todo set proper uri
+            uri: 'https://webhook.site/*',
+            tokenOptions: {
+              // The attached token should target this audience
+              // todo set proper audience uri
+              audience: 'https://dev-g60bne29.eu.auth0.com/api/v2/',
+            }
+          }
+        ]
+      }
     }),
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import {Component, Inject, Injectable} from '@angular/core';
 import { ModalController } from '../../components/modal/modal.controller';
 import { DOCUMENT } from '@angular/common';
 import { AuthService } from '@auth0/auth0-angular';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'littil-homepage',
@@ -10,6 +11,7 @@ import { AuthService } from '@auth0/auth0-angular';
 export class HomepageComponent {
   constructor(
     public auth: AuthService,
+    private http: HttpClient,
     @Inject(DOCUMENT) private doc: Document,
     private modalController: ModalController // todo not known if should be used in future
   ) {}
@@ -20,5 +22,11 @@ export class HomepageComponent {
 
   public logout() {
     this.auth.logout({ returnTo: this.doc.location.origin });
+  }
+
+  public call (){
+    // todo to test set your own uri
+    // todo when merging this should be gone
+    this.http.get("https://webhook.site/57e26b5e-72d6-42f5-80c5-137c59a50374").subscribe()
   }
 }
