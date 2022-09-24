bf610cfa8b303335223ef6a9d5e1e5f0d85117e0
L. Gijsen <lennertgijsen@home.nl>
2022-09-24 13:03:28 +0200

    LITTIL-95 Add ACM certificate

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Certificate(this, 'FrontendCertificate', {
+      domainName: 'acc.littil.org',
+      validation: CertificateValidation.fromDns(),
+    });
   }
 }
