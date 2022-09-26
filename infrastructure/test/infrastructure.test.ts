370ef9c0483989d72673600a61b9dda4aa47e62e
L. Gijsen <lennertgijsen@home.nl>
2022-09-26 21:34:12 +0200

    Write basic test for infrastructure

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,17 +1,32 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infrastructure from '../lib/infrastructure-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Website from '../lib/website-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infrastructure-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('S3 bucket and Cloudfront distribution created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const props = {
+    env: {
+      region: 'eu-west-1',
+    },
+    certificateArn: 'arn:aws:acm:us-east-1:123456789012:certificate/abcd1234-ab12-cd34-ef56-abcdefg12345',
+  };
+  const stack = new Website.WebsiteStack(app, 'WebsiteStack', props);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  // THEN
+  const template = Template.fromStack(stack);
+
+  template
+    .hasResourceProperties('AWS::S3::Bucket', {
+      BucketName: 'littil-acc-website',
+    });
+
+  template
+    .hasResourceProperties('AWS::CloudFront::Distribution', {
+      DistributionConfig: {
+        Aliases: [
+          'acc.littil.org',
+        ],
+      },
+    });
 });
