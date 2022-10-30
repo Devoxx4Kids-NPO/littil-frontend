0467b537b79d75d856c92a0823be18e7b705f77a
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2022-10-30 13:39:49 +0100

    LITTIL-95 Deploy website to S3 bucket

--- infrastructure/lib/website-stack.ts
+++ infrastructure/lib/website-stack.ts
@@ -1,11 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import { AllowedMethods, Distribution, PriceClass, ViewerProtocolPolicy, } from 'aws-cdk-lib/aws-cloudfront';
+import {
+  AllowedMethods,
+  Distribution,
+  PriceClass,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import {
+  Effect,
+  OpenIdConnectProvider,
+  Policy,
+  PolicyStatement,
+  Role,
+  WebIdentityPrincipal,
+} from 'aws-cdk-lib/aws-iam';
 import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-import { cloudfrontSpaErrorResponses } from "./cloudfront-spa-error-responses";
+import { cloudfrontSpaErrorResponses } from './cloudfront-spa-error-responses';
 
 export interface WebsiteStackProps extends cdk.StackProps {
   certificateArn: string;
@@ -16,7 +29,7 @@ export class WebsiteStack extends cdk.Stack {
     super(scope, id, props);
 
     const siteBucket = new Bucket(this, 'WebsiteS3Bucket', {
-      bucketName: 'littil-acc-website',
+      bucketName: 'littil-staging-website',
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
       removalPolicy: RemovalPolicy.DESTROY,
     });
@@ -40,5 +53,49 @@ export class WebsiteStack extends cdk.Stack {
 
     new CfnOutput(this, 'CloudfrontDistributionId', {value: distribution.distributionId});
     new CfnOutput(this, 'CloudfrontDomainName', {value: distribution.domainName});
+
+    /* Publish to S3 statement. */
+    const s3Statement = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        's3:PutObject',
+      ],
+      resources: [
+        siteBucket.bucketArn + '/*',
+      ],
+    });
+
+    /* Invalidate Cloudfront distribution statement. */
+    const cloudfrontStatement = new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        'cloudfront:CreateInvalidation',
+      ],
+      resources: [
+        'arn:aws:cloudfront::*:distribution/' + distribution.distributionId,
+      ],
+    });
+
+    const accountId = this.account;
+    const issuer = 'token.actions.githubusercontent.com';
+    const gitHubOrg = 'Devoxx4Kids-NPO';
+    const githubRepoName = 'littil-frontend';
+    const openIdConnectProviderArn = `arn:aws:iam::${accountId}:oidc-provider/${issuer}`;
+
+    const deploymentRole = new Role(this, 'WebsiteDeploymentRole', {
+      roleName: 'LITTIL-NL-staging-website-deploy',
+      assumedBy: new WebIdentityPrincipal(openIdConnectProviderArn, {
+        StringLike: {
+          [`${issuer}:sub`]: `repo:${gitHubOrg}/${githubRepoName}:*`,
+        },
+        StringEquals: {
+          [`${issuer}:aud`]: 'sts.amazonaws.com',
+        },
+      }),
+    });
+
+    const publishWebsitePolicy = new Policy(this, 'LITTIL-NL-staging-publish-policy');
+    publishWebsitePolicy.addStatements(s3Statement, cloudfrontStatement);
+    publishWebsitePolicy.attachToRole(deploymentRole);
   }
 }
