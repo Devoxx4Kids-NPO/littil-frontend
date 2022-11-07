6b0cb1a39e6ee1503eb0421a8a554d972cdbe4c7
Anja van Hagen <anja.vanhagen@ns.nl>
2022-11-07 22:59:02 +0100

    Add resolver to main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatorResolver } from './services/authentication.resolver';
 
 const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () =>
       import('./pages/admin/admin.module').then((m) => m.AdminModule),
+    resolve: {
+      auth: AuthenticatorResolver,
+    },
   },
   {
     path: '',
     loadChildren: () =>
       import('./pages/website/website.module').then((m) => m.WebsiteModule),
+    resolve: {
+      auth: AuthenticatorResolver,
+    },
   },
   {
     path: '**',
@@ -19,7 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
