fb9f98e34230e21548713a5f441741289f51c760
Anja van Hagen <a.vanhagen@gmail.com>
2023-01-14 11:44:45 +0100

    Add missing schoolName for schools profile

--- src/app/components/complete-profile-modal/complete-profile-modal.component.ts
+++ src/app/components/complete-profile-modal/complete-profile-modal.component.ts
@@ -5,9 +5,9 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, Observable, Subscription } from 'rxjs';
 import { GuestTeacherPostResource, School } from '../../api/generated';
 import { LittilSchoolService } from '../../services/littil-school/littil-school.service';
 import { LittilTeacherService } from '../../services/littil-teacher/littil-teacher.service';
@@ -38,14 +38,17 @@ import { IModalComponent } from '../modal/modal.controller';
   ],
 })
 export class CompleteProfileModalComponent
-  implements IModalComponent<undefined, undefined>, OnInit
+  implements IModalComponent<undefined, undefined>, OnInit, OnDestroy
 {
   close!: () => undefined;
   public loading = false;
+  public isSchool = false;
+  private roleSubscription!: Subscription;
   FormUtil = FormUtil;
 
   completeProfileForm: FormGroup = new FormGroup({
     role: new FormControl('', Validators.required),
+    schoolName: new FormControl('', Validators.required),
     firstName: new FormControl('', Validators.required),
     prefix: new FormControl(''),
     surname: new FormControl('', Validators.required),
@@ -63,13 +66,13 @@ export class CompleteProfileModalComponent
 
   roleChoices: RadioInput[] = [
     {
-      id: Roles.School,
-      description: 'Ik representeer een school',
-      checked: false,
-    },
-    {
       id: Roles.GuestTeacher,
       description: 'Ik ben een gastdocent',
+      checked: true,
+    },
+    {
+      id: Roles.School,
+      description: 'Ik representeer een school',
       checked: false,
     },
   ];
@@ -82,7 +85,24 @@ export class CompleteProfileModalComponent
     this.completeProfileForm.markAsUntouched();
   }
 
-  ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.completeProfileForm.controls['role'].valueChanges.subscribe(
+      (changes: any) => {
+        this.isSchool = changes === Roles.School;
+        if (changes === Roles.School) {
+          this.completeProfileForm.controls['schoolName'].enable();
+        } else {
+          this.completeProfileForm.controls['schoolName'].disable();
+        }
+      }
+    );
+  }
+
+  public ngOnDestroy(): void {
+    if (this.roleSubscription) {
+      this.roleSubscription.unsubscribe();
+    }
+  }
 
   public onClickSaveProfile(): Promise<boolean> {
     return Promise.resolve().then(() => {
@@ -91,26 +111,43 @@ export class CompleteProfileModalComponent
         return false;
       }
 
-      // TODO: check if houseNumber needs to be separated from street, because postalCode + housenumber equals an address and location
-      const formValues = {
-        firstName: this.completeProfileForm.controls['firstName'].value,
-        prefix: this.completeProfileForm.controls['prefix'].value,
-        surname: this.completeProfileForm.controls['surname'].value,
-        address:
-          this.completeProfileForm.controls['addressStreet'].value +
-          ' ' +
-          this.completeProfileForm.controls['addressHousenumber'].value,
-        postalCode:
-          this.completeProfileForm.controls['postalCodeNumbers'].value +
-          this.completeProfileForm.controls['postalCodeLetters'].value,
-      };
-
-      const createOrUpdateCall =
+      let createOrUpdateCall: Observable<GuestTeacherPostResource | School>;
+      if (
         this.completeProfileForm.controls['role'].value === Roles.GuestTeacher
-          ? this.guestTeacherService.createOrUpdate(
-              formValues as GuestTeacherPostResource
-            )
-          : this.schoolService.createOrUpdate(formValues as School);
+      ) {
+        const formValuesGuestTeacher: GuestTeacherPostResource = {
+          firstName: this.completeProfileForm.controls['firstName'].value,
+          prefix: this.completeProfileForm.controls['prefix'].value,
+          surname: this.completeProfileForm.controls['surname'].value,
+          address:
+            this.completeProfileForm.controls['addressStreet'].value +
+            ' ' +
+            this.completeProfileForm.controls['addressHousenumber'].value,
+          postalCode:
+            this.completeProfileForm.controls['postalCodeNumbers'].value +
+            this.completeProfileForm.controls['postalCodeLetters'].value,
+        };
+        createOrUpdateCall = this.guestTeacherService.createOrUpdate(
+          formValuesGuestTeacher
+        );
+      } else {
+        const formValuesSchool: School = {
+          name: this.completeProfileForm.controls['schoolName'].value,
+          firstName: this.completeProfileForm.controls['firstName'].value,
+          prefix: this.completeProfileForm.controls['prefix'].value,
+          surname: this.completeProfileForm.controls['surname'].value,
+          address:
+            this.completeProfileForm.controls['addressStreet'].value +
+            ' ' +
+            this.completeProfileForm.controls['addressHousenumber'].value,
+          postalCode:
+            this.completeProfileForm.controls['postalCodeNumbers'].value +
+            this.completeProfileForm.controls['postalCodeLetters'].value,
+        };
+        createOrUpdateCall =
+          this.schoolService.createOrUpdate(formValuesSchool);
+      }
+
       return firstValueFrom(createOrUpdateCall)
         .then(() => {
           this.close();
