00493bb7a2714b5ff3304fb00c99a65c238e9ed3
Kapeesh Manilal <kapeeshmanilal@gmail.com>
2023-08-01 08:51:39 +0200

    LITTIL-185 (LITTIL-201) : Exploring open street maps

--- src/app/pages/admin/search/map-options.ts
+++ src/app/pages/admin/search/map-options.ts
@@ -1,87 +1,18 @@
+import { latLng, tileLayer } from 'leaflet'
+
 export enum MapZoomLevels {
   WHOLE_NL = 8,
   LOCATION_FOCUS = 15,
   SELECTION_FOCUS = 17,
 }
 
-export const MAP_OPTIONS: google.maps.MapOptions = {
+export const MAP_OPTIONS: L.MapOptions = {
+  layers: [
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom:MapZoomLevels.SELECTION_FOCUS })
+  ],
+  zoom:  MapZoomLevels.WHOLE_NL,
+  center: latLng(52.1326, 5.2913),
   zoomControl: true,
-  scrollwheel: true,
-  disableDefaultUI: true,
-  keyboardShortcuts: false,
   maxZoom: MapZoomLevels.SELECTION_FOCUS,
   minZoom: MapZoomLevels.WHOLE_NL,
-  styles: [
-    {
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'on',
-        },
-      ],
-    },
-    {
-      featureType: 'administrative',
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'landscape',
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'landscape.man_made',
-      elementType: 'labels.text',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'poi',
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'road',
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'on',
-        },
-      ],
-    },
-    {
-      featureType: 'transit',
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'water',
-      elementType: 'labels',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-  ],
 };
