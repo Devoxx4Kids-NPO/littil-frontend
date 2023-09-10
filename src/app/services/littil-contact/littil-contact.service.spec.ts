785549ab0b18ec16959cc48ee85793a0ef2551c1
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2023-09-10 16:07:07 +0200

    test coverage

---
+++ src/app/services/littil-contact/littil-contact.service.spec.ts
@@ -0,0 +1,36 @@
+import {createHttpFactory, HttpMethod, SpectatorHttp} from "@ngneat/spectator";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ContactPostResource, ContactService} from "../../api/generated";
+import {LittilContactService} from "./littil-contact.service";
+
+describe('LittilContactService', () => {
+  let baseUrl = 'http://localhost:8080';
+  let spectator: SpectatorHttp<LittilContactService>;
+  const createHttp = createHttpFactory({
+    service: LittilContactService,
+    imports: [HttpClientTestingModule],
+    providers: [ContactService],
+  });
+
+  beforeEach(() => {
+    spectator = createHttp();
+  });
+
+  it('should be created', () => {
+    expect(spectator.service).toBeTruthy();
+  });
+
+  describe('sendEmail', () => {
+    it('should send email', () => {
+      spectator.service
+        .sendEmail({
+          recipient: "recipient",
+          medium: 'contactInfo',
+          message: 'message',
+        } as ContactPostResource)
+        .subscribe();
+      spectator.expectOne(baseUrl + '/api/v1/contacts', HttpMethod.POST);
+    });
+  });
+
+});
