9236cc9d3c571b0ffa4dcf731681313d5e5d56e1
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2023-09-13 23:02:54 +0200

    Don't use property when local variable is sufficient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { IMenuItem, menuRoutes, MenuType } from './pages/menu.routes';
 import { PermissionController } from './services/permission.controller';
-import  feedbackfin from "feedbackfin";
-import {getLittilConfigFromWindow} from "../littilConfig";
+import feedbackfin from 'feedbackfin';
+import { getLittilConfigFromWindow } from '../littilConfig';
 
 const littilConfig = getLittilConfigFromWindow();
 
@@ -14,18 +14,21 @@ const littilConfig = getLittilConfigFromWindow();
 })
 export class AppComponent implements OnInit {
   public menuRoutes: IMenuItem[] = menuRoutes;
-  window:any;
 
   constructor(
     public readonly permissionController: PermissionController,
     @Inject(DOCUMENT) public document: Document
-  ) {}
+  ) {
+  }
 
   public ngOnInit(): void {
-    this.window = this.document.defaultView
-    this.window.feedbackfin = { ...feedbackfin };
-    this.window.feedbackfin.config.url = littilConfig.apiHost + "/api/v1/feedback";
-    this.window.feedbackfin.config.mode = "form";
+    const littilFeedbackFin = {
+      ...feedbackfin,
+    };
+    littilFeedbackFin.config.url = littilConfig.apiHost + '/api/v1/feedback';
+    littilFeedbackFin.config.mode = 'form';
+
+    (this.document.defaultView as any).feedbackfin = littilFeedbackFin;
 
     this.permissionController.onPermissionChange.subscribe(() => {
       const adminPages: IMenuItem[] = menuRoutes.filter(
