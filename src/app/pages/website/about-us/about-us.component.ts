a51ee629a6fc213cb2dbef9c3c1ba8f99c0e1a7b
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2023-09-19 21:08:29 +0200

    Add pictures and infra lead

--- src/app/pages/website/about-us/about-us.component.ts
+++ src/app/pages/website/about-us/about-us.component.ts
@@ -7,37 +7,48 @@ import { Router } from '@angular/router';
 })
 export class AboutUsComponent {
 
-  managementMock: { name: string, role: string }[] = [
+  managementMock: { name: string, role: string, image?: string }[] = [
     {
       name: 'Koen Aerts',
-      role: 'Voorzitter Devoxx4Kids Nederland'
+      role: 'Voorzitter Devoxx4Kids Nederland',
+      image: 'koen.jpg',
     },
     {
       name: 'Saskia Vermeer Ooms',
-      role: 'Secretaris Devoxx4Kids Nederland'
+      role: 'Secretaris Devoxx4Kids Nederland',
+      image: 'saskia.jpg',
     },
     {
       name: 'Eddy Vos',
-      role: 'Penningmeester Devoxx4Kids Nederland'
+      role: 'Penningmeester Devoxx4Kids Nederland',
+      image: 'eddy.jpg',
     },
     {
       name: 'Eddy Vos',
-      role: 'Product Owner / Scrum Master Devoxx4Kids LITTIL'
+      role: 'Product Owner / Scrum Master Devoxx4Kids LITTIL',
+      image: 'eddy.jpg',
     },
     {
       name: 'Pepijn Schildkamp',
-      role: 'Lead Back-End Developers Devoxx4Kids Nederland'
+      role: 'Lead Back-End Developers Devoxx4Kids Nederland',
+      image: 'pepijn.jpg',
     },
     {
       name: 'Anja van Hagen',
-      role: 'Lead Front-End Developers Devoxx4Kids Nederland'
+      role: 'Lead Front-End Developers Devoxx4Kids Nederland',
+      image: 'anja.jpg',
+    },
+    {
+      name: 'Lennert Gijsen',
+      role: 'Lead Infra Devoxx4Kids Nederland',
+      image: 'lennert.jpg',
     },
   ];
 
   constructor(private router: Router) {
   }
 
-  navToContact(): void {
-    this.router.navigateByUrl('');
+    navToContact(): void {
+    this.router.navigateByUrl('contact');
   }
 }
