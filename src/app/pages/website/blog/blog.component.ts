f114ffe36e621d5784927e82efc5118a27ffd9a8
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2023-09-21 22:12:47 +0200

    LITTIL-227 redirect message in html style and code style/formatting

--- src/app/pages/website/blog/blog.component.ts
+++ src/app/pages/website/blog/blog.component.ts
@@ -2,12 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'redirect',
-  template: 'redirecting...'
+  templateUrl: './blog.component.html',
 })
 export class BlogComponent implements OnInit {
-  constructor() { }
-
   ngOnInit() {
-    window.location.href = 'https://www.devoxx4kids.org/nederland/'
+    window.location.href = 'https://www.devoxx4kids.org/nederland/';
   }
 }
--- src/app/pages/website/blog/blog.module.ts
+++ src/app/pages/website/blog/blog.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from "@angular/router";
-import {BlogComponent} from "./blog.component";
+import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog.component';
+import { ContentContainerModule } from "../../../components/content-container/content-container.module";
+import {TitleModule} from "../../../components/title/title.module";
 
 const routes: Routes = [
   {
@@ -14,7 +16,9 @@ const routes: Routes = [
   declarations: [BlogComponent],
     imports: [
         CommonModule,
+        ContentContainerModule,
         RouterModule.forChild(routes),
+        TitleModule,
     ]
 })
 export class BlogModule {
