051a89469e267d640a16d3c638dd04ae388da5b4
Tubby <git@tubby.nl>
2023-09-24 13:27:07 +0200

    cleanup contact-banner component, extra text on sponsors added to P above

--- src/app/components/contact-banner/contact-banner.component.ts
+++ src/app/components/contact-banner/contact-banner.component.ts
@@ -6,32 +6,6 @@ import {Router} from "@angular/router";
   templateUrl: './contact-banner.component.html',
 })
 export class ContactBannerComponent {
-  @Input() bannerText: string = 'Wij komen dan graag in contact met jou!'
-  @Input() linkWord: string = 'contact'
-
-  get prefixText(): string {
-    return this.getBannerTextPart('prefixText')
-  }
-  get suffixText(): string {
-    return this.getBannerTextPart('suffixText')
-  }
-  get linkText(): string {
-    return this.getBannerTextPart('linkText')
-  }
-
-  public getBannerTextPart(part: 'prefixText'|'suffixText'|'linkText'): string {
-    if (this.bannerText.includes(this.linkWord)) {
-      let components = this.bannerText.split(this.linkWord)
-      const parsedText = {
-        'prefixText': components[0],
-        'suffixText': components[1],
-        'linkText': this.linkWord
-      }
-      return parsedText[part]
-    }
-    return ''
-  }
-
   constructor(private router: Router) {
   }
 
