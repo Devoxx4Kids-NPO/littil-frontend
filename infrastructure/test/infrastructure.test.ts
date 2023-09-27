1ff528f8a73c1f004f542042b86468f8d00cde71
Lennert Gijsen <l.gijsen@gijsensoftware.nl>
2023-09-27 20:45:21 +0200

    Fix test compilation

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,9 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import * as Website from '../lib/website-stack';
+import { CertificatesStack } from '../lib/certificates-stack';
+import { WebsiteStack } from '../lib/website-stack';
 
 test('S3 bucket and Cloudfront distribution created', () => {
   const app = new cdk.App();
+
+  const certProps = {
+    domains: ['local.littil.org'],
+  };
+  const certificateStack = new CertificatesStack(app, 'WebsiteStack', certProps);
+
   // WHEN
   const props = {
     littilEnvironment: 'test',
@@ -11,9 +18,9 @@ test('S3 bucket and Cloudfront distribution created', () => {
     env: {
       region: 'eu-west-1',
     },
-    certificateArn: 'arn:aws:acm:us-east-1:123456789012:certificate/abcd1234-ab12-cd34-ef56-abcdefg12345',
+    certificate: certificateStack.certificate
   };
-  const stack = new Website.WebsiteStack(app, 'WebsiteStack', props);
+  const stack = new WebsiteStack(app, 'WebsiteStack', props);
 
   // THEN
   const template = Template.fromStack(stack);
