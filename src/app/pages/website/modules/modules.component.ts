3886d9e21d7544e900b42760a60f630aa3a24111
ooms <saskia.vermeerooms@gmail.com>
2024-06-18 15:03:48 +0200

    LITTIL-3090-ModulesPage-processed review comments

--- src/app/pages/website/modules/modules.component.ts
+++ src/app/pages/website/modules/modules.component.ts
@@ -7,43 +7,7 @@ import { Router } from '@angular/router';
 })
 export class ModulesComponent {
 
-  managementMock: { name: string, role: string, image?: string }[] = [
-    {
-      name: 'Koen Aerts',
-      role: 'Voorzitter Devoxx4Kids Nederland',
-      image: 'koen.jpg',
-    },
-    {
-      name: 'Saskia Vermeer Ooms',
-      role: 'Secretaris Devoxx4Kids Nederland',
-      image: 'saskia.jpg',
-    },
-    {
-      name: 'Eddy Vos',
-      role: 'Penningmeester Devoxx4Kids Nederland / Product owner LITTIL',
-      image: 'eddy.jpg',
-    },
-    {
-      name: 'Pepijn Schildkamp',
-      role: 'Bestuurslid Devoxx4Kids Nederland',
-      image: 'pepijn.jpg',
-    },
-    {
-      name: 'Anja van Hagen',
-      role: 'Lead Front-End Developers LITTIL',
-      image: 'anja.jpg',
-    },
-    {
-      name: 'Lennert Gijsen',
-      role: 'Lead Infra LITTIL',
-      image: 'lennert.jpg',
-    },
-    {
-      name: 'Marcel Wildenburg',
-      role: 'Lead Back-End Developers LITTIL',
-      image: 'marcel.jpg',
-    },
-  ];
+  
 
   constructor(private router: Router) {
   }
