002f551e553a0d864ca16db5b855ef6fe3d09e16
Dick Dijk <dick@dijkrosoft.nl>
2025-04-10 09:27:08 +0200

    Links were not working

--- src/app/pages/website/info/information/information.component.ts
+++ src/app/pages/website/info/information/information.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ContentContainerComponent } from '../../../../components/content-container/content-container.component';
 import { FooterComponent } from '../../../../components/footer/footer.component';
 import { TitleComponent } from '../../../../components/title/title.component';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'littil-info',
   templateUrl: './information.component.html',
   standalone: true,
-  imports: [TitleComponent, ContentContainerComponent, FooterComponent],
+  imports: [TitleComponent, ContentContainerComponent, FooterComponent, RouterLink],
 })
 export class InformationComponent {}
