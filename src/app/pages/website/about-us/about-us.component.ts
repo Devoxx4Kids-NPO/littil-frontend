56a6916c04856d0bab286535b78d9d08bcbb293f
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2025-05-14 22:27:27 +0200

    remove board members from about-us (Over ons) page

--- src/app/pages/website/about-us/about-us.component.ts
+++ src/app/pages/website/about-us/about-us.component.ts
@@ -1,10 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { ButtonComponent } from '../../../components/button/button.component';
 import { ContactBannerComponent } from '../../../components/contact-banner/contact-banner.component';
 import { ContentContainerComponent } from '../../../components/content-container/content-container.component';
-import { FooterComponent } from '../../../components/footer/footer.component';
 import { TitleComponent } from '../../../components/title/title.component';
 
 @Component({
@@ -12,56 +9,10 @@ import { TitleComponent } from '../../../components/title/title.component';
   templateUrl: './about-us.component.html',
   standalone: true,
   imports: [
-    ButtonComponent,
     CommonModule,
     ContentContainerComponent,
     TitleComponent,
-    FooterComponent,
     ContactBannerComponent,
   ],
 })
-export class AboutUsComponent {
-  managementMock: { name: string; role: string; image?: string }[] = [
-    {
-      name: 'Koen Aerts',
-      role: 'Voorzitter Devoxx4Kids Nederland',
-      image: 'koen.jpg',
-    },
-    {
-      name: 'Saskia Vermeer Ooms',
-      role: 'Secretaris Devoxx4Kids Nederland',
-      image: 'saskia.jpg',
-    },
-    {
-      name: 'Eddy Vos',
-      role: 'Penningmeester Devoxx4Kids Nederland / Product owner LITTIL',
-      image: 'eddy.jpg',
-    },
-    {
-      name: 'Pepijn Schildkamp',
-      role: 'Bestuurslid Devoxx4Kids Nederland',
-      image: 'pepijn.jpg',
-    },
-    {
-      name: 'Anja van Hagen',
-      role: 'Lead Front-End Developers LITTIL',
-      image: 'anja.jpg',
-    },
-    {
-      name: 'Lennert Gijsen',
-      role: 'Lead Infra LITTIL',
-      image: 'lennert.jpg',
-    },
-    {
-      name: 'Marcel Wildenburg',
-      role: 'Lead Back-End Developers LITTIL',
-      image: 'marcel.jpg',
-    },
-  ];
-
-  constructor(private router: Router) {}
-
-  navToContact(): void {
-    this.router.navigateByUrl('contact');
-  }
-}
+export class AboutUsComponent {}
