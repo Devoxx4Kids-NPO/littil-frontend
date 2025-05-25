a5994d4ded747f0af98f3e77f42b81b0b4fb5a6e
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2025-05-25 21:32:28 +0200

    update admin menu - extend with auth0 details

--- src/app/pages/admin/admin/admin.component.spec.ts
+++ src/app/pages/admin/admin/admin.component.spec.ts
@@ -1,25 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
-import { ActivatedRouteStub } from '@ngneat/spectator';
-import { ContactBannerComponent } from '../../../components/contact-banner/contact-banner.component';
 import { ContentContainerComponent } from '../../../components/content-container/content-container.component';
 import { TitleComponent } from '../../../components/title/title.component';
 import { AdminComponent } from './admin.component';
+import { UserStatistics } from "../../../api/generated";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { MockProvider } from "ng-mocks";
+import { LittilUserService } from "../../../services/littil-user/littil-user.service";
+import { of } from "rxjs";
+import { By } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+
+const userStatistics: UserStatistics[] = [
+  {
+    authorizationType: 'user',
+    count: 3,
+    lastCreated: '23-05-2025'
+  },
+];
+
+function getAdminServiceDef() {
+  return {
+    imports: [
+      AdminComponent,
+      CommonModule,
+      ContentContainerComponent,
+      TitleComponent,
+    ],
+    schemas: [NO_ERRORS_SCHEMA],
+    providers: [
+      MockProvider(LittilUserService, {
+        getUserStatistics: () => of(userStatistics),
+      }),
+    ],
+  };
+}
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AdminComponent, TitleComponent, ContentContainerComponent, ContactBannerComponent],
-      providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: ActivatedRouteStub,
-        },
-      ],
-    }).compileComponents();
+    await TestBed.configureTestingModule(getAdminServiceDef()).compileComponents();
 
     fixture = TestBed.createComponent(AdminComponent);
     component = fixture.componentInstance;
@@ -28,5 +49,11 @@ describe('AdminComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display statistics when data is available', () => {
+    fixture.detectChanges();
+    const rows = fixture.debugElement.queryAll(By.css('tbody tr'));
+    expect(rows.length).toBe(1);
   });
 });
