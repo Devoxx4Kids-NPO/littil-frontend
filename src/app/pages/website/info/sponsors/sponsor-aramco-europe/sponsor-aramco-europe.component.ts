772f2a8b53036468f0a6d0943444f3231557119f
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2025-09-11 21:54:34 +0200

    new sponsor Aramco Europe

---
+++ src/app/pages/website/info/sponsors/sponsor-aramco-europe/sponsor-aramco-europe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'littil-sponsor-aramco-europe',
+  standalone: true,
+  imports: [],
+  templateUrl: './sponsor-aramco-europe.component.html',
+  styleUrl: './sponsor-aramco-europe.component.scss'
+})
+export class SponsorAramcoEuropeComponent {
+
+}
