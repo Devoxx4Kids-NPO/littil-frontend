db787706b9757ecd04a056364f24f960fbf0d0c0
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2025-09-11 22:31:19 +0200

    expand information

--- src/app/pages/website/info/sponsors/sponsor-aramco-europe/sponsor-aramco-europe.component.ts
+++ src/app/pages/website/info/sponsors/sponsor-aramco-europe/sponsor-aramco-europe.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'littil-sponsor-aramco-europe',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './sponsor-aramco-europe.component.html',
   styleUrl: './sponsor-aramco-europe.component.scss'
 })
 export class SponsorAramcoEuropeComponent {
-
+  expanded = false;
 }
