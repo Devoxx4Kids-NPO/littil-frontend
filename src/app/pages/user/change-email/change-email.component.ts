d22b0312c8df5f0bd01f3e3e91680a6587e9bad7
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2025-10-05 22:39:15 +0200

    WIP change-email component

--- src/app/pages/user/change-email/change-email.component.ts
+++ src/app/pages/user/change-email/change-email.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ProfileContainerComponent } from '../../../components/profile-container/profile-container.component';
 
 @Component({
@@ -6,7 +8,22 @@ import { ProfileContainerComponent } from '../../../components/profile-container
   templateUrl: './change-email.component.html',
   standalone: true,
   imports: [
+	  CommonModule,
+	  FormsModule,
 	  ProfileContainerComponent,
    ],
 })
-export class ChangeEmailComponent { }
+export class ChangeEmailComponent { 
+  newEmail: string = '';
+  verificationCode: string = '';
+  codeRequested: boolean = false;
+
+  requestVerificationCode() {
+    // Call your backend to send the code
+    this.codeRequested = true;
+  }
+
+  submitEmailChange() {
+    // Send newEmail and verificationCode to backend
+  }
+}
